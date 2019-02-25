b130d840c96632fa686fe616ad9c99691fc84225
Rish <zrishabhgarg@gmail.com>
2019-02-25 11:25:45 +0700

    Updated config/settings use

--- core/server/lib/members/index.js
+++ core/server/lib/members/index.js
@@ -211,7 +211,7 @@ module.exports = function MembersApi({
     httpHandler.staticRouter = staticRouter;
     httpHandler.apiRouter = apiRouter;
     httpHandler.memberUserObject = users;
-    httpHandler.updateSubscription = function (paymentConfig) {
+    httpHandler.reconfigureSettings = function (paymentConfig) {
         subscriptions = new Subscriptions(paymentConfig);
         users = Users({
             subscriptions,
