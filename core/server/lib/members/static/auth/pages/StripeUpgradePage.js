ebcc330c8136429561fbb1da0ac15a533946ee1e
Zimo <peter.zimon@gmail.com>
2019-02-25 18:35:11 +0700

    Styled upgrade page

--- core/server/lib/members/static/auth/pages/StripeUpgradePage.js
+++ core/server/lib/members/static/auth/pages/StripeUpgradePage.js
@@ -56,13 +56,13 @@ export default class StripeSubscriptionPage extends Component {
         const selectedPlanId = this.state.selectedPlan ? this.state.selectedPlan.id : "";
         const dollarAmount = (amount / 100);
         return (
-            <div class="gm-plan">
-                <input type="radio" id={id} name="radio-group" value={id} defaultChecked={id === selectedPlanId} />
-                <label for={id}>
-                    <span class="gm-amount">{`$${dollarAmount}`}</span>
-                    <span class="gm-interval">{`${interval}`}</span>
-                </label>
-            </div>
+            <label for={ id }>
+                <div className={ (selectedPlanId === id ? "gm-plan selected" : "gm-plan") }>
+                    <input type="radio" id={ id } name="radio-group" value={ id } defaultChecked={ id === selectedPlanId } />
+                    <span className="gm-amount">{ `$${dollarAmount}` }</span>
+                    <span className="gm-interval"><span className="gm-currency">{ `${currency}` }</span> { `${interval}` }</span>
+                </div>
+            </label>
         )
     }
 
@@ -75,7 +75,7 @@ export default class StripeSubscriptionPage extends Component {
 
     renderPlans(plans) {
         return (
-            <div onChange={(e) => this.changePlan(e)}>
+            <div className="mt3" onChange={(e) => this.changePlan(e)}>
                 {
                     plans.map((plan) => this.renderPlan(plan))
                 }
@@ -86,6 +86,7 @@ export default class StripeSubscriptionPage extends Component {
     renderPlansSection() {
         return (
             <div className="gm-plans-container">
+                <h2 className="gm-form-section">Billing period</h2>
                 {this.renderPlans(this.plans)}
             </div>
         )
@@ -94,20 +95,19 @@ export default class StripeSubscriptionPage extends Component {
     render({ error, handleSubmit, stripeConfig }) {
         const publicKey = stripeConfig.config.publicKey || '';
         return (
-            <div className="flex">
-                sdafa
+            <div class="gm-upgrade-page">
                 <div className="gm-modal-form gm-subscribe-form">
-                    <FormHeader title="Subscribe" error={error} errorText="Unable to confirm payment" />
-                    <div className="flex">
+                    <FormHeader title="Upgrade" error={error} errorText="Unable to confirm payment" />
+                    <div className="flex flex-column justfiy-stretch mt7">
+                        { this.renderPlansSection() }
+                        <div className="mt4 nb3">
+                            <h2 className="gm-form-section">Card details</h2>
+                        </div>
                         <StripeProvider apiKey={publicKey}>
                             <Elements>
                                 <PaymentFormWrapped handleSubmit={handleSubmit} publicKey={publicKey} selectedPlan={this.state.selectedPlan} />
                             </Elements>
                         </StripeProvider>
-                        <div className="flex justify-center mt4">
-                            <FormHeaderCTA title="Already a member?" label="Log in" hash="#signin" />
-                        </div>
-                        { this.renderPlansSection() }
                     </div>
                 </div>
             </div>
