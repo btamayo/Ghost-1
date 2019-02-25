90c07144f4f8404d42609782776aa986dedf4ba5
Zimo <peter.zimon@gmail.com>
2019-02-25 20:33:05 +0700

    Update Stripe error styles

--- core/server/lib/members/static/auth/components/CheckoutForm.js
+++ core/server/lib/members/static/auth/components/CheckoutForm.js
@@ -1,25 +1,31 @@
-import React, {Component} from 'react';
-import {CardElement} from 'react-stripe-elements';
+import React, { Component } from 'react';
+import { CardElement } from 'react-stripe-elements';
 
 class CheckoutForm extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    let style = {
-      base: {
-        '::placeholder': {
-          color: '#aab7c4'
-        }
-      }
-    };
-    return (
-      <div className="gm-form-element">
-        <CardElement style={style} />
-      </div>
-    );
-  }
+    render() {
+        let style = {
+            base: {
+                '::placeholder': {
+                    color: '#B8C2CC',
+                    fontSize: '15px'
+                }
+            },
+            invalid: {
+                '::placeholder': {
+                    color: 'rgba(240, 82, 48, 0.75)'
+                }
+            }
+        };
+        return (
+            <div className="gm-form-element">
+                <CardElement style={ style } />
+            </div>
+        );
+    }
 }
 
 export default CheckoutForm;
